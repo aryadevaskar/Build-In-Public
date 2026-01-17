f99c12360ddf984666c9f95dc0408726eda46092
aryadevaskar <aryadevaskar@gmail.com>
2026-01-17 14:37:23 +0530

    Added little wins folder

---
+++ Little Wins/src/imports/LittleWins.tsx
@@ -0,0 +1,281 @@
+import svgPaths from "./svg-c4adqmk6um";
+import imgGeminiGeneratedImage28Owa628Owa628OwRemovebgPreview1 from "figma:asset/36d8841d1218a1466587ae874cbb7d64a79abbb8.png";
+import imgMusicCat from "figma:asset/4cd1ed791c3818c3d00b0666382f2b8e98f830cb.png";
+import imgWorkingCat from "figma:asset/42e82da45431ecad768f6045ca20ad90ae6f60c3.png";
+import imgSleepyCat from "figma:asset/b814ba93cb65eabf07d482ff0d3868dc61acb7ee.png";
+import imgGardenerCat from "figma:asset/a9903360529ebd184ff76d6559afa5a6f3bba76f.png";
+
+function DailyChecklistCard() {
+  return (
+    <div className="absolute bg-[#fffaf3] h-[627px] left-[640px] overflow-clip shadow-[0px_4px_11.7px_0px_rgba(151,103,49,0.27)] top-[102px] w-[547px]" data-name="Daily_Checklist_Card">
+      <p className="absolute css-ew64yg font-['Playpen_Sans:Bold',sans-serif] font-bold leading-[normal] left-[40px] text-[#5c3b27] text-[34.012px] top-[26px]">17th January</p>
+      <p className="absolute css-ew64yg font-['Playpen_Sans:Regular',sans-serif] font-normal leading-[normal] left-[80px] text-[#815439] text-[22px] top-[110px]">squash 3 tiny bugs</p>
+      <p className="[text-decoration-skip-ink:none] absolute css-ew64yg decoration-solid font-['Playpen_Sans:Regular',sans-serif] font-normal leading-[normal] left-[80px] line-through text-[#815439] text-[22px] top-[167px]">clean up my figma layers</p>
+      <p className="absolute css-ew64yg font-['Playpen_Sans:Regular',sans-serif] font-normal leading-[normal] left-[80px] text-[#815439] text-[22px] top-[228px]">sketch a new sticker idea</p>
+      <p className="absolute css-ew64yg font-['Playpen_Sans:Regular',sans-serif] font-normal leading-[normal] left-[80px] text-[#815439] text-[22px] top-[289px]">{`update my project "read me"`}</p>
+      <p className="absolute css-ew64yg font-['Playpen_Sans:Regular',sans-serif] font-normal leading-[normal] left-[80px] text-[#815439] text-[22px] top-[350px]">{`finally name my 'frame 452'`}</p>
+      <p className="absolute css-ew64yg font-['Playpen_Sans:Bold',sans-serif] font-bold leading-[normal] left-[490px] text-[#5c3b27] text-[14.005px] top-[594px]">2026</p>
+      <div className="absolute h-0 left-[38px] top-[86px] w-[364px]">
+        <div className="absolute inset-[-0.5px_0]">
+          <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 364 1">
+            <path d="M0 0.5H364" id="Vector 4" stroke="var(--stroke-0, #5C3B27)" />
+          </svg>
+        </div>
+      </div>
+      <div className="absolute h-[150px] left-[386px] top-[-7px] w-[181px]" data-name="Gemini_Generated_Image_28owa628owa628ow-removebg-preview 1">
+        <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgGeminiGeneratedImage28Owa628Owa628OwRemovebgPreview1} />
+      </div>
+      <div className="absolute inset-[18.34%_88.3%_77.83%_6.95%]" data-name="istockphoto-2177795345-612x612 1 (Traced)">
+        <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 26 24">
+          <path d={svgPaths.p3de2a800} id="istockphoto-2177795345-612x612 1 (Traced)" stroke="var(--stroke-0, #5C3B27)" strokeWidth="1.5" />
+        </svg>
+      </div>
+      <div className="absolute inset-[27.43%_88.3%_68.74%_6.95%]" data-name="istockphoto-2177795345-612x612 1 (Traced)">
+        <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 26 24">
+          <path d={svgPaths.p3de2a800} fill="var(--fill-0, #5C3B27)" id="istockphoto-2177795345-612x612 1 (Traced)" stroke="var(--stroke-0, #5C3B27)" strokeWidth="1.5" />
+        </svg>
+      </div>
+      <div className="absolute inset-[37.16%_88.3%_59.01%_6.95%]" data-name="istockphoto-2177795345-612x612 1 (Traced)">
+        <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 26 24">
+          <path d={svgPaths.p3de2a800} id="istockphoto-2177795345-612x612 1 (Traced)" stroke="var(--stroke-0, #5C3B27)" strokeWidth="1.5" />
+        </svg>
+      </div>
+      <div className="absolute inset-[46.89%_88.3%_49.28%_6.95%]" data-name="istockphoto-2177795345-612x612 1 (Traced)">
+        <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 26 24">
+          <path d={svgPaths.p3de2a800} id="istockphoto-2177795345-612x612 1 (Traced)" stroke="var(--stroke-0, #5C3B27)" strokeWidth="1.5" />
+        </svg>
+      </div>
+      <div className="absolute inset-[56.62%_88.3%_39.55%_6.95%]" data-name="istockphoto-2177795345-612x612 1 (Traced)">
+        <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 26 24">
+          <path d={svgPaths.p3de2a800} id="istockphoto-2177795345-612x612 1 (Traced)" stroke="var(--stroke-0, #5C3B27)" strokeWidth="1.5" />
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+function CelebrateButton() {
+  return (
+    <div className="absolute left-[51px] size-[24px] top-[24px]" data-name="Celebrate_Button">
+      <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 24 24">
+        <g id="Celebrate_Button">
+          <path d={svgPaths.p2fcb600} fill="var(--fill-0, #5C3B27)" id="Vector" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Share() {
+  return (
+    <div className="absolute left-[51px] size-[24px] top-[71px]" data-name="Share">
+      <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 24 24">
+        <g id="Share">
+          <path d={svgPaths.p3b154800} fill="var(--fill-0, #5C3B27)" id="Vector" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function ShapesHeart() {
+  return (
+    <div className="absolute left-[53px] size-[24px] top-[118px]" data-name="Shapes / heart">
+      <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 24 24">
+        <g id="Shapes / heart">
+          <path d={svgPaths.p2dc77e00} fill="var(--fill-0, #5C3B27)" id="Vector" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Frame1() {
+  return (
+    <div className="absolute bg-[#fff7ec] h-[411px] left-[-38px] overflow-clip rounded-[10px] shadow-[0px_4px_11.7px_0px_rgba(151,103,49,0.27)] top-[113px] w-[92px]">
+      <div className="absolute flex h-[122px] items-center justify-center left-[64px] top-[232px] translate-x-[-50%] w-[38px]" style={{ "--transform-inner-width": "67.921875", "--transform-inner-height": "19" } as React.CSSProperties}>
+        <div className="flex-none rotate-[270deg]">
+          <p className="css-ew64yg font-['Playpen_Sans:SemiBold',sans-serif] font-semibold leading-[normal] relative text-[#815439] text-[25.304px] text-center">little wins</p>
+        </div>
+      </div>
+      <div className="absolute flex inset-[89.54%_18.48%_4.14%_55.43%] items-center justify-center">
+        <div className="flex-none h-[24px] rotate-[270deg] w-[26px]">
+          <div className="relative size-full" data-name="istockphoto-2177795345-612x612 1 (Traced)">
+            <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 26 24">
+              <path clipRule="evenodd" d={svgPaths.p38449dc0} fill="var(--fill-0, #815439)" fillRule="evenodd" id="istockphoto-2177795345-612x612 1 (Traced)" />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <CelebrateButton />
+      <Share />
+      <ShapesHeart />
+    </div>
+  );
+}
+
+function StickerLibrary() {
+  return (
+    <div className="absolute contents left-[42px] top-[467px]" data-name="Sticker_Library">
+      <div className="absolute flex h-[299.428px] items-center justify-center left-[42px] top-[467px] w-[314.838px]" style={{ "--transform-inner-width": "0", "--transform-inner-height": "19" } as React.CSSProperties}>
+        <div className="flex-none rotate-[330deg]">
+          <div className="h-[203.787px] relative w-[245.887px]" data-name="Music_Cat">
+            <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgMusicCat} />
+          </div>
+        </div>
+      </div>
+      <div className="absolute h-[228px] left-[193px] top-[505px] w-[275px]" data-name="Working_Cat">
+        <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgWorkingCat} />
+      </div>
+      <div className="absolute h-[234px] left-[99px] top-[587px] w-[282px]" data-name="Sleepy_Cat">
+        <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgSleepyCat} />
+      </div>
+      <div className="absolute h-[203px] left-[138px] top-[518px] w-[245px]" data-name="Gardener_Cat">
+        <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgGardenerCat} />
+      </div>
+      <div className="absolute h-[210px] left-[155px] top-[563px] w-[253px]" data-name="Rocket_Cat">
+        <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgGeminiGeneratedImage28Owa628Owa628OwRemovebgPreview1} />
+      </div>
+    </div>
+  );
+}
+
+function MemoryJarContainer() {
+  return (
+    <div className="absolute h-[234px] left-[444px] top-[495px] w-[136px]" data-name="Memory_Jar_Container">
+      <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 136 234">
+        <g id="Memory_Jar_Container">
+          <path clipRule="evenodd" d={svgPaths.p1999cb00} fill="var(--fill-0, #5C3B27)" fillRule="evenodd" id="3272d143f407f9baac4e63befb83aec4 1 (Traced)" />
+          <g id="3272d143f407f9baac4e63befb83aec4 1 (Traced)_2">
+            <path clipRule="evenodd" d={svgPaths.p2e56a80} fill="url(#paint0_radial_1_94)" fillRule="evenodd" />
+            <path clipRule="evenodd" d={svgPaths.p2e2f4080} fill="url(#paint1_radial_1_94)" fillRule="evenodd" />
+            <path d={svgPaths.p2151ba00} fill="url(#paint2_radial_1_94)" />
+            <path clipRule="evenodd" d={svgPaths.p1f0d2980} fill="url(#paint3_radial_1_94)" fillRule="evenodd" />
+            <path clipRule="evenodd" d={svgPaths.pe10ce00} fill="url(#paint4_radial_1_94)" fillRule="evenodd" />
+          </g>
+          <g id="Jar_Glow_FX">
+            <path d={svgPaths.p1847c4f1} fill="var(--fill-0, #FFF3E4)" id="Vector 5" />
+            <path d={svgPaths.p20034f00} fill="var(--fill-0, #FFF3E4)" id="Vector 6" />
+            <path d={svgPaths.p3e6078d0} fill="var(--fill-0, #FFF3E4)" id="Vector 7" />
+          </g>
+        </g>
+        <defs>
+          <radialGradient cx="0" cy="0" gradientTransform="translate(68 117) rotate(90) scale(117 68)" gradientUnits="userSpaceOnUse" id="paint0_radial_1_94" r="1">
+            <stop stopColor="#5C3B27" stopOpacity="0" />
+            <stop offset="1" stopColor="#F78E00" stopOpacity="0.14" />
+          </radialGradient>
+          <radialGradient cx="0" cy="0" gradientTransform="translate(68 117) rotate(90) scale(117 68)" gradientUnits="userSpaceOnUse" id="paint1_radial_1_94" r="1">
+            <stop stopColor="#5C3B27" stopOpacity="0" />
+            <stop offset="1" stopColor="#F78E00" stopOpacity="0.14" />
+          </radialGradient>
+          <radialGradient cx="0" cy="0" gradientTransform="translate(68 117) rotate(90) scale(117 68)" gradientUnits="userSpaceOnUse" id="paint2_radial_1_94" r="1">
+            <stop stopColor="#5C3B27" stopOpacity="0" />
+            <stop offset="1" stopColor="#F78E00" stopOpacity="0.14" />
+          </radialGradient>
+          <radialGradient cx="0" cy="0" gradientTransform="translate(68 117) rotate(90) scale(117 68)" gradientUnits="userSpaceOnUse" id="paint3_radial_1_94" r="1">
+            <stop stopColor="#5C3B27" stopOpacity="0" />
+            <stop offset="1" stopColor="#F78E00" stopOpacity="0.14" />
+          </radialGradient>
+          <radialGradient cx="0" cy="0" gradientTransform="translate(68 117) rotate(90) scale(117 68)" gradientUnits="userSpaceOnUse" id="paint4_radial_1_94" r="1">
+            <stop stopColor="#5C3B27" stopOpacity="0" />
+            <stop offset="1" stopColor="#F78E00" stopOpacity="0.14" />
+          </radialGradient>
+        </defs>
+      </svg>
+    </div>
+  );
+}
+
+function Frame() {
+  return (
+    <div className="absolute bg-[#ffd295] h-[299px] left-[120px] overflow-clip shadow-[-5px_5px_4px_0px_rgba(83,46,24,0.25)] text-[17.864px] top-[102px] w-[467px]">
+      <p className="absolute css-ew64yg font-['Playpen_Sans:Medium',sans-serif] font-medium leading-[normal] left-[167px] text-[#5c3b27] top-[17px]">{`Kitty's Logbook`}</p>
+      <ul className="absolute block css-g0mm18 font-['Playpen_Sans:Regular',sans-serif] font-normal leading-[0] left-[21px] text-[#915633] top-[66px]">
+        <li className="css-4hzbpn mb-0 ms-[26.796px]">
+          <span className="leading-[40px]">Sent 3 emails. Expecting 3 treats in return.</span>
+        </li>
+        <li className="css-4hzbpn ms-[26.796px]">
+          <span className="leading-[40px]">Gentle reminder: You are doing great, human.</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function Group() {
+  return (
+    <div className="absolute contents left-[120px] top-[102px]" data-name="Group">
+      <Frame />
+    </div>
+  );
+}
+
+function SendToJarButton() {
+  return (
+    <div className="absolute left-[561px] size-[51px] top-[82px]" data-name="Send_To_Jar_Button">
+      <div className="absolute inset-[-15.1%_-22.94%_-30.78%_-22.94%]">
+        <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 74.4 74.4">
+          <g id="Send_To_Jar_Button">
+            <g filter="url(#filter0_d_1_89)" id="Ellipse 2">
+              <circle cx="37.2" cy="33.2" fill="var(--fill-0, #FFFAF3)" r="25.5" />
+            </g>
+            <g clipPath="url(#clip0_1_89)" id="User Interface / Paper Plane">
+              <path d={svgPaths.p2e02d280} fill="var(--fill-0, #815439)" id="Vector" />
+            </g>
+          </g>
+          <defs>
+            <filter colorInterpolationFilters="sRGB" filterUnits="userSpaceOnUse" height="74.4" id="filter0_d_1_89" width="74.4" x="0" y="0">
+              <feFlood floodOpacity="0" result="BackgroundImageFix" />
+              <feColorMatrix in="SourceAlpha" result="hardAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" />
+              <feOffset dy="4" />
+              <feGaussianBlur stdDeviation="5.85" />
+              <feComposite in2="hardAlpha" operator="out" />
+              <feColorMatrix type="matrix" values="0 0 0 0 0.592529 0 0 0 0 0.403636 0 0 0 0 0.19113 0 0 0 0.27 0" />
+              <feBlend in2="BackgroundImageFix" mode="normal" result="effect1_dropShadow_1_89" />
+              <feBlend in="SourceGraphic" in2="effect1_dropShadow_1_89" mode="normal" result="shape" />
+            </filter>
+            <clipPath id="clip0_1_89">
+              <rect fill="white" height="24" transform="translate(20.7293 32.7) rotate(-45)" width="24" />
+            </clipPath>
+          </defs>
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+function StickyNoteGroup() {
+  return (
+    <div className="absolute contents left-[120px] top-[82px]" data-name="Sticky_Note_Group">
+      <Group />
+      <SendToJarButton />
+    </div>
+  );
+}
+
+export default function LittleWins() {
+  return (
+    <div className="bg-[#ffecd3] relative size-full" data-name="little wins">
+      <div className="absolute flex inset-[2.4%_1.33%_80.09%_86.8%] items-center justify-center">
+        <div className="flex-none h-[117px] rotate-[15deg] w-[126px]">
+          <div className="relative size-full" data-name="istockphoto-2177795345-612x612 1 (Traced)">
+            <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 126 117">
+              <path clipRule="evenodd" d={svgPaths.p1b764e80} fill="var(--fill-0, #815439)" fillRule="evenodd" id="istockphoto-2177795345-612x612 1 (Traced)" />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <DailyChecklistCard />
+      <p className="absolute css-ew64yg font-['Playpen_Sans:SemiBold',sans-serif] font-semibold leading-[normal] left-[253px] text-[#5c3b27] text-[39.2px] text-center top-[458px] translate-x-[-50%]">my mood</p>
+      <Frame1 />
+      <StickerLibrary />
+      <MemoryJarContainer />
+      <StickyNoteGroup />
+      <div className="absolute h-[24px] left-[458px] top-[694px] w-[111px]">
+        <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 111 24">
+          <ellipse cx="55.5" cy="12" fill="var(--fill-0, #FFD295)" id="Ellipse 3" rx="55.5" ry="12" />
+        </svg>
+      </div>
+    </div>
+  );
+}
